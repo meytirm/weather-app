b9e036000aa7f10900e4bfdbee41e347f62e2880
Mehdi Rahmani <mahdi0131rain@gmail.com>
2022-08-01 00:59:56 +0430

    Add dark mode, title, date

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,28 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 import { useIcon } from '@/composables/icon'
+import { useQuasar } from 'quasar'
+import type { LocationModel } from '@/models/weather'
+import { useMeta } from 'quasar'
 
-const drawerLeft = ref(false)
+useMeta({
+  title: 'Weather',
+})
+
+// const drawerLeft = ref(false)
+const timeNow = ref<LocationModel | null>(null)
+const $q = useQuasar()
+const darkMode = ref($q.dark.isActive)
+
+function toggleDarkMode(isDark: boolean | 'auto') {
+  $q.dark.set(isDark)
+}
+
+function dateLoaded(date: LocationModel) {
+  timeNow.value = date
+}
+
+toggleDarkMode(darkMode.value)
 useIcon()
 </script>
 
@@ -12,46 +32,56 @@ useIcon()
       view="hHh Lpr lff"
       container
       style="height: 100vh"
-      class="shadow-2 rounded-borders"
+      class="shadow-2"
     >
-      <q-header elevated class="bg-black">
+      <q-header elevated class="bg-indigo-10-custom">
         <q-toolbar>
-          <q-btn
-            flat
-            @click="drawerLeft = !drawerLeft"
-            round
-            dense
-            icon="menu"
-          />
+          <!--          <q-btn-->
+          <!--            flat-->
+          <!--            @click="drawerLeft = !drawerLeft"-->
+          <!--            round-->
+          <!--            dense-->
+          <!--            icon="menu"-->
+          <!--          />-->
           <q-toolbar-title>Header</q-toolbar-title>
+          <q-toolbar-title v-if="timeNow">{{
+            timeNow.localtime
+          }}</q-toolbar-title>
+          <q-toggle
+            label="dark mode"
+            model-value="darkMode"
+            v-model="darkMode"
+            @click="toggleDarkMode(darkMode)"
+          />
         </q-toolbar>
       </q-header>
 
-      <q-drawer
-        v-model="drawerLeft"
-        show-if-above
-        :width="200"
-        :breakpoint="700"
-        elevated
-        class="bg-primary text-white"
-      >
-        <q-scroll-area class="fit">
-          <div class="q-pa-sm">
-            <div v-for="n in 50" :key="n">Drawer {{ n }} / 50</div>
-          </div>
-        </q-scroll-area>
-      </q-drawer>
+      <!--      <q-drawer-->
+      <!--        dark-->
+      <!--        class="bg-indigo-10-custom"-->
+      <!--        v-model="drawerLeft"-->
+      <!--        show-if-above-->
+      <!--        :width="200"-->
+      <!--        :breakpoint="700"-->
+      <!--        elevated-->
+      <!--      >-->
+      <!--        <q-scroll-area class="fit">-->
+      <!--          <div class="q-pa-sm">-->
+      <!--            <div v-for="n in 10" :key="n">Drawer {{ n }} / 50</div>-->
+      <!--          </div>-->
+      <!--        </q-scroll-area>-->
+      <!--      </q-drawer>-->
 
       <q-page-container>
-        <q-page padding class="flex flex-center">
-          <RouterView />
+        <q-page class="flex flex-center">
+          <RouterView @date-loaded="dateLoaded" />
         </q-page>
       </q-page-container>
     </q-layout>
   </div>
 </template>
 
-<style>
+<style lang="scss">
 @import '@/assets/base.css';
 
 @media (hover: hover) {
@@ -62,5 +92,9 @@ useIcon()
 
 nav a:first-of-type {
   border: 0;
+}
+
+.blur-background {
+  filter: blur(1.5px);
 }
 </style>
