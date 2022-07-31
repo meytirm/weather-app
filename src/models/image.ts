179818572ab3db38dda5c78e350042d34d097ef4
Mehdi Rahmani <mahdi0131rain@gmail.com>
2022-08-01 01:05:51 +0430

    Create a interface for image service data

---
+++ src/models/image.ts
@@ -0,0 +1,16 @@
+export interface ImageModel {
+  height: number
+  urls: UrlsModel
+  width: number
+  description: string
+  alt_description: string
+}
+
+interface UrlsModel {
+  raw: string
+  full: string
+  regular: string
+  small: string
+  small_s3: string
+  thumb: string
+}
