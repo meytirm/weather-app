60bf3529e51400eccc4212dde8e58b1ae4b73d3f
Mehdi Rahmani <mahdi0131rain@gmail.com>
2022-08-01 01:00:31 +0430

    Create axios config file

---
+++ src/services/axiosConfig.ts
@@ -0,0 +1,51 @@
+import axios, { AxiosInstance, AxiosRequestConfig } from 'axios'
+
+const axiosInstance: AxiosInstance = axios.create({
+  timeout: 1000,
+  headers: {
+    Accept: 'application/json',
+    'Content-Type': 'application/json',
+  },
+})
+
+const apiService = {
+  get<ResponseType, RequestType = never>(
+    url: string,
+    config: AxiosRequestConfig = {}
+  ) {
+    return axiosInstance.get<ResponseType, RequestType>(url, config)
+  },
+
+  post<ResponseType, RequestType = never>(
+    url: string,
+    data: RequestType,
+    config: AxiosRequestConfig = {}
+  ) {
+    return axiosInstance.post<ResponseType, RequestType>(url, data, config)
+  },
+
+  put<ResponseType, RequestType = never>(
+    url: string,
+    data: RequestType,
+    config: AxiosRequestConfig = {}
+  ) {
+    return axiosInstance.put<ResponseType, RequestType>(url, data, config)
+  },
+
+  delete<ResponseType, RequestType = never>(
+    url: string,
+    config: AxiosRequestConfig = {}
+  ) {
+    return axiosInstance.delete<ResponseType, RequestType>(url, config)
+  },
+
+  patch<ResponseType, RequestType = never>(
+    url: string,
+    data: RequestType,
+    config: AxiosRequestConfig = {}
+  ) {
+    return axiosInstance.patch<ResponseType, RequestType>(url, data, config)
+  },
+}
+
+export default apiService
