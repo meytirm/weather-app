76c7f183381a379fbabca7e88619c76d8cf7a80e
Mehdi Rahmani <mahdi0131rain@gmail.com>
2022-08-02 20:03:42 +0430

    Set a better name

--- src/services/weather.ts
+++ src/services/weather.ts
@@ -1,11 +1,11 @@
 import apiService from '@/services/axiosConfig'
 
-function weatherService(coordinates: string) {
+function weatherService(placeName: string) {
   const accessKey = import.meta.env.VITE_WEATHER_ACCESS_KEY
   const baseUrl = import.meta.env.VITE_WEATHER_URL
 
   return apiService.get(
-    `${baseUrl}current?access_key=${accessKey}&query=${coordinates}&units=m`
+    `${baseUrl}current?access_key=${accessKey}&query=${placeName}&units=m`
   )
 }
 
