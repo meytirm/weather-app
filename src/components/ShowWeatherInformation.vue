c4473f28f2106052bee7a50399db67db8b02bcfd
Mehdi Rahmani <mahdi0131rain@gmail.com>
2022-08-05 00:14:31 +0430

    Add type to proptype

--- src/components/ShowWeatherInformation.vue
+++ src/components/ShowWeatherInformation.vue
@@ -76,7 +76,8 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, PropType } from 'vue'
+import { defineComponent } from 'vue'
+import type { PropType } from 'vue'
 import type { ImageModel } from '@/models/image'
 
 export default defineComponent({
